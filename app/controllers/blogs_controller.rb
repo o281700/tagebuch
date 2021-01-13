ded4b67cea0bde2e27b67aa4970ff428c15ed68d
o281700 <siba0311@gmail.com>
2021-01-13 23:43:43 +0900

    controller_action_new/create

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,8 +3,21 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
   end
 
   def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :article)
   end
 end
