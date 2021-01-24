70ee912cb3ad30f8ba54fb35b95c016a206464f3
o281700 <siba0311@gmail.com>
2021-01-24 20:05:26 +0900

    change_commentController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to blog_path(comment.blog)
+    else
+      render 'blogs/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, blog_id: params[:blog_id])
+  end
 end
