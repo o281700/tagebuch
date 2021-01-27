79af44a433a0e59488a7aa5ca98adc221ca7f5bd
o281700 <siba0311@gmail.com>
2021-01-27 19:28:19 +0900

    chang_comment_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#comment' do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    it 'textが存在すれば保存できること' do
+      expect(@comment).to be_valid
+    end
+
+    it 'textが空であれば保存できないこと' do
+      @comment.text = ''
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Text can't be blank")
+    end
+
+    it 'userに紐づいていない場合は保存できないこと' do
+      @comment.user = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include('User must exist')
+    end
+
+    it 'blogに紐づいていない場合は保存できないこと' do
+      @comment.blog = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include('Blog must exist')
+    end
+  end
 end
