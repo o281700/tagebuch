fad286f8445b7e7d9309511bae20a19c968f77ae
o281700 <siba0311@gmail.com>
2021-01-28 02:57:12 +0900

    add_routes_destroy

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   }
   root 'top#index'
   resources :blogs do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
 end
